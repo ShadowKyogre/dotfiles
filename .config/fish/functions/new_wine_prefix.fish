1c660f256c16e052d054fdfd1e6421647082773a
ShadowKyogre <shadowkyogre@aim.com>
2014-10-11 20:46:42 -0700

    Update tag_search and fix new_wine_prefix var expansion

--- .config/fish/functions/new_wine_prefix.fish
+++ .config/fish/functions/new_wine_prefix.fish
@@ -1,10 +1,10 @@
 function new_wine_prefix
 	if test (count argv) -eq 0
 		echo "Sorry, can't make another wine prefix without a parameter"
-	        return 1
+		return 1
 	else if test (count argv) -eq 1
-		env WINEPREFIX=~/.local/share/wineprefixes/"{$argv[1]}" WINEARCH="win32" wine wineboot
+		env WINEPREFIX=$HOME/.local/share/wineprefixes/"$argv[1]" WINEARCH="win32" wine wineboot
 	else
-		env WINEPREFIX=~/.local/share/wineprefixes/"{$argv[1]}" WINEARCH="{$argv[2]}" wine wineboot
+		env WINEPREFIX=$HOME/.local/share/wineprefixes/"$argv[1]" WINEARCH="$argv[2]" wine wineboot
 	end
 end
--- .config/fish/functions/tag_search.fish
+++ .config/fish/functions/tag_search.fish
@@ -1,5 +1,6 @@
 function tag_search
 	set all_tags (implode $argv '.*')
-	echo "[\[^[\]*$all_tags\.*].*\$"
+	# echo "[\[^[\]*$all_tags\.*].*\$"
+	# above is constructed regex for debugging
 	ls .|grep "[\[^[\]*$all_tags.*].*\$"
 end
