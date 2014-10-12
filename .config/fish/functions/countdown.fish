52d4f85c15013447302da942bce42114147d0394
ShadowKyogre <shadowkyogre@aim.com>
2014-10-11 22:07:53 -0700

    Added support for playing music for a specified time (it was driving me nuts that I had pure silence while doing writing exercises)

--- .config/fish/functions/countdown.fish
+++ .config/fish/functions/countdown.fish
@@ -1,19 +1,33 @@
 function countdown
-	if test (count $argv) -eq 0
+	set time_stuff (explode $argv[1] :)
+
+	if test (count $time_stuff) -eq 0
 		printf "%sDon't have all the time vars set!%s" (set_color -o red) (set_color normal)>&2
 		return 1
-	else if test (count $argv) -eq 1
-		set secs $argv[1]
-	else if test (count $argv) -eq 2
-		set secs (math $argv[1] \* 60 + $argv[2] )
+	else if test (count $time_stuff) -eq 1
+		set secs $time_stuff[1]
+	else if test (count $time_stuff) -eq 2
+		set secs (math $time_stuff[1] \* 60 + $time_stuff[2] )
 	else
-		set secs (math $argv[1] \* 3600 + $argv[2] \* 60 + $argv[3] )
+		set secs (math $time_stuff[1] \* 3600 + $time_stuff[2] \* 60 + $time_stuff[3] )
 	end
+
+	if test (count $argv) -eq 2
+		smplayer -actions "compact play" $argv[2] >/dev/null ^/dev/null &
+	end
+
 	while test $secs -gt 0
 		printf "\r%02d:%02d:%02d" (math $secs / 3600) (math \($secs / 60\) \% 60) (math $secs \% 60)
 		set secs (math $secs - 1)
 		sleep 1
 	end
+
+	#jobs
+	#echo %1 (pidof smplayer)
+	if test (count $argv) -eq 2
+		kill -s SIGTERM %2
+	end
+
 	printf "\r%02d:%02d:%02d" (math $secs / 3600) (math \($secs / 60\) \% 60) (math $secs \% 60)
-	ffplay -autoexit ~/Music/Bayonetta/Bayonetta_-_Lets_Dance_Boys.mp3 -ss 3 -t 71.5 -nodisp -hide_banner
+	ffplay -autoexit $ALARM_SOUND -nodisp -hide_banner
 end
