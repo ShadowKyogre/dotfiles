ca2d34c358c988fdc2e6e218798acad350f3ccbc
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-01-08 00:04:37 -0800

    Migrate mode indicator into host+user display

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,29 @@
-function fish_mode_prompt --description "Displays the current mode"
+function fish_mode_prompt
+end
+
+function fish_vi_mode_display
 	# Do nothing if not in vi mode
 	if set -q __fish_vi_mode
 		switch $fish_bind_mode
 			case default
 				set_color --bold --background red white
-				echo '[N]'
+				echo -n "$argv[1]"
 			case insert
 				set_color --bold --background blue white
-				echo '[I]'
+				echo -n "$argv[1]"
 			case visual
 				set_color --bold --background magenta white
-				echo '[V]'
+				echo -n "$argv[1]"
 		end
 		set_color normal
-		echo -n ' '
 	end
 end
 
 function fish_prompt
-	printf " %s_____/[%s" (set_color purple) (set_color -o red)
-	printf "%s@%s" $USER (hostname)
+	printf "%s _____/[%s" (set_color purple) (set_color -o red)
+	fish_vi_mode_display (printf "%s@%s" $USER (hostname))
 	printf "%s%s|%s%s" (set_color normal) (set_color purple) (set_color -o red) (date "+%F %T")
-	printf "%s%s]\______________)\n    (_/[%s%s" (set_color normal) (set_color purple) (set_color -o red) (prompt_pwd)
+	printf "%s%s]\______________)\n(_/[%s%s" (set_color normal) (set_color purple) (set_color -o red) (prompt_pwd)
 	printf '%s%s]\_____________/(%s' (set_color normal) (set_color purple) (set_color -o red)
 	if test -d .git
 		printf "(%s%s%s)" (set_color yellow) (git rev-parse --abbrev-ref HEAD) (set_color -o red)
