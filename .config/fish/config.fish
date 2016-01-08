6d665c5eb29927606e375cc314524a88b503f315
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-01-07 23:34:29 -0800

    Adjust fish prompt for vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ function fish_prompt
 	printf " %s_____/[%s" (set_color purple) (set_color -o red)
 	printf "%s@%s" $USER (hostname)
 	printf "%s%s|%s%s" (set_color normal) (set_color purple) (set_color -o red) (date "+%F %T")
-	printf "%s%s]\______________)\n(_/[%s%s" (set_color normal) (set_color purple) (set_color -o red) (prompt_pwd)
+	printf "%s%s]\______________)\n    (_/[%s%s" (set_color normal) (set_color purple) (set_color -o red) (prompt_pwd)
 	printf '%s%s]\_____________/(%s' (set_color normal) (set_color purple) (set_color -o red)
 	if test -d .git
 		printf "(%s%s%s)" (set_color yellow) (git rev-parse --abbrev-ref HEAD) (set_color -o red)
@@ -31,3 +31,5 @@ complete -c burp -xa '(__fish_complete_suffix src.tar.gz)'
 complete -c qpdfview -xa '(__fish_complete_suffix pdf)'
 complete -c lowriter -xa '(__fish_complete_suffix odt)'
 complete -c localc -xa '(__fish_complete_suffix ods)'
+
+fish_vi_mode
