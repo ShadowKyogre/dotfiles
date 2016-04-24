aa9ab285a88ead630fd97ba446a33f27951441a5
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-04-24 11:03:55 -0700

    Put the while loop inside the function

--- .config/fish/functions/breakcycle.fish
+++ .config/fish/functions/breakcycle.fish
@@ -1,13 +1,25 @@
 function breakcycle --description 'Reinforces when to take a break'
 	if set -q TMUX
-		tmux set-window-option -q @bcycle-paused 0
+		tmux rename-window breakcycle
 	end
-	termdown 2h -c 5 -a -f 3x5 -T "Computer time"
-	notify-send "Get off your seat" -t 20000
 
-	if set -q TMUX
-		tmux set-window-option -q @bcycle-paused 0
+	while true
+		if set -q TMUX
+			tmux set-window-option -q @bcycle-paused 0
+		end
+		termdown 2h -c 5 -a -f 3x5 -T "Computer time"
+		notify-send "Get off your seat" -t 20000
+		if set -q TMUX
+			tmux-notify-all.sh "Get off your seat"
+		end
+
+		if set -q TMUX
+			tmux set-window-option -q @bcycle-paused 0
+		end
+		termdown 10m -c 5 -a -f 3x5 -T "Exercise time"
+		notify-send "You can get back in seat" -t 20000
+		if set -q TMUX
+			tmux-notify-all.sh "You can get back in seat"
+		end
 	end
-	termdown 10m -c 5 -a -f 3x5 -T "Exercise time"
-	notify-send "You can get back in seat" -t 20000
 end
