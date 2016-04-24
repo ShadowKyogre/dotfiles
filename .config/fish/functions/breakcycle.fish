8e1170c42c37e2ddb03e0e95fe95cf2507c29403
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-04-24 11:13:11 -0700

    Add option to set first time to be different

--- .config/fish/functions/breakcycle.fish
+++ .config/fish/functions/breakcycle.fish
@@ -3,11 +3,18 @@ function breakcycle --description 'Reinforces when to take a break'
 		tmux rename-window breakcycle
 	end
 
+	set first_loop 1
+
 	while true
 		if set -q TMUX
 			tmux set-window-option -q @bcycle-paused 0
 		end
-		termdown 2h -c 5 -a -f 3x5 -T "Computer time"
+		if test \( "$first_loop" -eq 1 \) -a \( -n "$argv[1]" \)
+			termdown "$argv[1]" -c 5 -a -f 3x5 -T "Computer time"
+			set first_loop 0
+		else
+			termdown 2h -c 5 -a -f 3x5 -T "Computer time"
+		end
 		notify-send "Get off your seat" -t 20000
 		if set -q TMUX
 			tmux-notify-all.sh "Get off your seat"
