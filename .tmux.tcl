f497ad073cf5b566fe0e609796ec9ac47cac30e0
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-04-27 09:02:09 -0700

    Refactor tmux tcl to use procs
    Why use procs? So I don't clutter the session variable namespace.  Also,
    I added a kill menu because there's no default keybinding for that in
    tmux.

--- .tmux.tcl
+++ .tmux.tcl
@@ -1,7 +1,7 @@
 #set tcl
 
-bind-key A tcl {
-	set cur_vol  [exec ponymix get-volume]
+proc volume_manager {} {
+	variable cur_vol  [exec ponymix get-volume]
 
 	choose-from-list \
 		-tag set-volume -val "volume: $cur_vol" \
@@ -20,9 +20,11 @@ bind-key A tcl {
 	}
 }
 
-bind-key a tcl {
-	set cur_repeat [exec sh -c "cmus-remote -Q|grep repeat_current|cut -d' ' -f3"]
-	set cur_status [exec sh -c "cmus-remote -Q|grep status|cut -d' ' -f2"]
+
+
+proc cmus_control {} {
+	variable cur_repeat [exec sh -c "cmus-remote -Q|grep repeat_current|cut -d' ' -f3"]
+	variable cur_status [exec sh -c "cmus-remote -Q|grep status|cut -d' ' -f2"]
 
 	choose-from-list \
 		-tag "play/pause"     -val "play/pause ($cur_status)" \
@@ -48,21 +50,38 @@ bind-key a tcl {
 	}
 }
 
-bind-key C tcl {
+proc session_manager {} {
+	variable items [output-of-list { list-sessions -F "#S" } ]
+
 	choose-from-list \
 	-val "<<new>>" \
 	-val "<<new w/a pwd>>" \
 	-val "<<attach w/a pwd>>" \
-	-list [output-of-list { list-sessions -F "#S" } ] \
+	-val "<<kill>>" \
+	-list $items \
 	-onselect {
 		if { "$_" eq "<<new>>" } {
 			command-prompt -p "new-session name" "new-session -A -s %1"
 		} elseif { "$_" eq "<<new w/a pwd>>" } {
-			command-prompt -p "new-session name,new-session pwd" -I,~ "new-session -A -s %1 -c %2"
+			command-prompt -p "new-session name,new-session pwd" -I,~ "new-session -A -s %1 -c \[file normalize %2\]"
 		} elseif { "$_" eq "<<attach w/a pwd>>" } {
-			command-prompt -p "session pwd" -I~ "attach-session -t %1 -c %2"
+			choose-from-list -list $items \
+			-onselect {
+				command-prompt -p "session pwd" -I~ "attach-session -t $_ -c \[file normalize %2\]"
+			}
+			
+		} elseif { "$_" eq "<<kill>>" } {
+			choose-from-list -list $items \
+			-onselect {
+				kill-session -t "$_"
+			}
 		} else {
 			attach-session -t "$_"
 		}
 	}
 }
+
+
+bind-key A tcl volume_manager
+bind-key a tcl cmus_control
+bind-key C tcl session_manager
