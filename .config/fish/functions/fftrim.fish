32a40371298383d20674824b01463e483fee04c0
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-03 14:10:45 -0700

    If filename has no .'s, then it has no extensions

--- .config/fish/functions/fftrim.fish
+++ .config/fish/functions/fftrim.fish
@@ -2,7 +2,7 @@ function fftrim
 	set path_ext (splitext "$argv[1]")
 	set path_no_ext (basename "$argv[1]" "$path_ext")
 	set path_dir (dirname "$argv[1]")
-	set trimmed_path "$path_dir/$path_no_ext.trimmed$path_ext"
+	set trimmed_path "$path_dir/$path_no_ext.trimmed.$path_ext"
 
 	ffmpeg -i "$argv[1]" -ss "$argv[2]" -t "$argv[3]" "$trimmed_path"
 end
--- .config/fish/functions/splitext.fish
+++ .config/fish/functions/splitext.fish
@@ -3,5 +3,9 @@ function splitext
 	if expr "$fname" : '\.' > /dev/null
 		set fname (expr "$fname" : "\.\(.*\)" )
 	end
-	echo "$fname"|grep -Eo '\.[^.]+$'
+	if not expr "$fname" : ".*\..*" > /dev/null
+		echo
+	else
+		echo "$fname"|awk -F. '{print $NF}'
+	end
 end
