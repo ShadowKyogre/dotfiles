feb452c80c6dcaa914595df769d1d8a77f890b88
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-05 15:16:58 -0700

    Brighten colors, more sensible characters
    There's no reason for dirty and invalid to use unicode when x and + are
    sufficient. Unstaged also makes more sense as a !.

--- .config/fish/functions/fish_git_settings.fish
+++ .config/fish/functions/fish_git_settings.fish
@@ -4,16 +4,18 @@ function fish_git_settings
 	set -g __fish_git_prompt_showupstream verbose
 	set -g __fish_git_prompt_show_informative_status 'yes'
 	set -g __fish_git_prompt_showuntrackedfiles 'yes'
-	set -g __fish_git_prompt_char_upstream_behind '<'
-	set -g __fish_git_prompt_char_upstream_ahead '>'
+	set -g __fish_git_prompt_char_untrackedfiles '?'
+	set -g __fish_git_prompt_char_dirtystate '!'
+	set -g __fish_git_prompt_char_invalidstate 'x'
 
 
 	set -g __fish_git_prompt_color purple
 	set -g __fish_git_prompt_color_branch yellow -o
 	set -g __fish_git_prompt_color_upstream blue -o
+	set -g __fish_git_prompt_color_untrackedfiles purple -o
 	set -g __fish_git_prompt_color_stateseparator blue
 	set -g __fish_git_prompt_color_stashstate cyan
-	set -g __fish_git_prompt_color_dirtystate yellow
+	set -g __fish_git_prompt_color_dirtystate yellow -o
 	set -g __fish_git_prompt_color_stagedstate green
 	set -g __fish_git_prompt_color_cleanstate green
 end
