e39c8cfc9a1ba39d2e5c615c5bc0bd0fdca491b4
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-06 18:14:17 -0700

    Add percol theming

---
+++ .config/percol.rc.py
@@ -0,0 +1,4 @@
+percol.view.PROMPT = '<on_red><bold><yellow>QUERY:</yellow></bold></on_red> %q'
+percol.view.RPROMPT = '<on_red><bold><yellow>(%i/%I) [%n/%N]</yellow></bold></on_red>'
+percol.view.CANDIDATES_LINE_SELECTED = ('underline', 'bold', 'on_red', 'yellow')
+percol.view.CANDIDATES_LINE_BASIC = ('bold', 'on_magenta', 'white')
