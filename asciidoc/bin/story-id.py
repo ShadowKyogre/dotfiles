37d161a95c639160891e27a86964697afdcd649a
ShadowKyogre <shadowkyogre.public@gmail.com>
2016-05-14 18:52:14 -0700

    Add story id generator
    Why? Because I'm horrible at naming things. I also end up writing out of
    sequence, so.... Yeah :|. It's so that way I can just get started on
    writing w/o worrying about naming until later.

---
+++ asciidoc/bin/story-id.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+import os
+import random
+import sys
+
+MAX_LEN = 9
+
+urandom = random.SystemRandom()
+rnum = urandom.randint(0, 63**MAX_LEN-1)
+
+ALPHABET='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LEN_ALPHA=len(ALPHABET)
+
+def base62(a):
+	baseit = (
+		lambda a=a, b=LEN_ALPHA: (not a) and '0' or
+		baseit(a-a%b, b*LEN_ALPHA) + ALPHABET[a%b%(LEN_ALPHA-1) or -1*bool(a%b)]
+	)
+	return baseit()
+
+#http://stackoverflow.com/a/36875787
+
+result = base62(rnum).lstrip('0').zfill(MAX_LEN)
+
+if len(sys.argv) == 2:
+	print(result, end='')
+else:
+	print(result)
