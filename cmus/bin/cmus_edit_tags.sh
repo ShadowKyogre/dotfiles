7d446d99f22886fefa30467e348f2371734bd407
ShadowKyogre <shadowkyogre.public@gmail.com>
2020-11-22 00:53:42 -0800

    cmus: termite -> kitty to use tag editor

--- cmus/bin/cmus_edit_tags.sh
+++ cmus/bin/cmus_edit_tags.sh
@@ -3,7 +3,7 @@ file="$(cmus-remote -C 'echo {}')"
 
 if [ -f "$file" ]
 then
-	 termite -t "Editing tags for $file" -e "$HOME/bin/mutagen_urwid.py $(printf '%q' "$file")" &
+	kitty -T "Editing tags for ${file}" -e "${HOME}/bin/mutagen_urwid.py" "${file}" &
 else
-	 echo "Oop, couldn't find selected track" >&2
+	echo "Oop, couldn't find selected track" >&2
 fi
