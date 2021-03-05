5710492d97051ec3306c95f257f0183ff41dd742
ShadowKyogre <shadowkyogre.public@gmail.com>
2021-03-05 13:10:30 -0800

    plasma: Set env vars specific to plasma

---
+++ plasma/.config/plasma-workspace/env/vars.sh
@@ -0,0 +1,5 @@
+export PYTHONSTARTUP="${HOME}/.config/python/config.py"
+export ZDOTDIR="${HOME}/.config/zsh"
+export EDITOR=gvim
+export PATH="${HOME}/bin:${HOME}/.pub-cache/bin/:/opt/flutter/bin/cache/dart-sdk/bin:${PATH}"
+export VIEB_WINDOW_FRAME=1
