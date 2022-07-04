63c92219d3e838e1ecc3ebc4554492c863042f26
ShadowKyogre <shadowkyogre.public@gmail.com>
2022-07-04 10:18:24 -0700

    plasma: use GUI prompt for key adds

--- plasma/.config/plasma-workspace/env/vars.sh
+++ plasma/.config/plasma-workspace/env/vars.sh
@@ -4,3 +4,4 @@ export EDITOR=gvim
 export PATH="${HOME}/bin:${HOME}/.pub-cache/bin/:/opt/flutter/bin/cache/dart-sdk/bin:${PATH}"
 export VIEB_WINDOW_FRAME=1
 [ -z "$SSH_AGENT_PID" ] && eval "$(ssh-agent -s)"
+export SSH_ASKPASS=/usr/bin/ksshaskpass
