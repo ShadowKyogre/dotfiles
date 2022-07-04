51e4f8c18c9a1d3868300c247c580cb872e8d2e1
ShadowKyogre <shadowkyogre.public@gmail.com>
2022-07-04 09:58:22 -0700

    plasma: add ssh-agent integration

--- plasma/.config/plasma-workspace/env/vars.sh
+++ plasma/.config/plasma-workspace/env/vars.sh
@@ -3,3 +3,4 @@ export ZDOTDIR="${HOME}/.config/zsh"
 export EDITOR=gvim
 export PATH="${HOME}/bin:${HOME}/.pub-cache/bin/:/opt/flutter/bin/cache/dart-sdk/bin:${PATH}"
 export VIEB_WINDOW_FRAME=1
+[ -z "$SSH_AGENT_PID" ] && eval "$(ssh-agent -s)"
